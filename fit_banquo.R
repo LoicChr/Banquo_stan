50b559342d5b9af0f75a2055e4491abe1fefa451
Daniel B. Stouffer <dbstouffer@gmail.com>
2018-07-17 11:02:06 -0600

    changed some settings so that the code works in *this* repo; needed to use system libs instead of BH and RcppEigen; increased sample size

--- fit_banquo.R
+++ fit_banquo.R
@@ -1,60 +1,54 @@
 
-setwd("~/Desktop/R-Desktop/Banquo2")
+# setwd("~/Desktop/R-Desktop/Banquo2")
 rm(list=ls())
 
 # libraries and options for them
 library(rstan)
 rstan_options(auto_write = TRUE)
 
-# to install the package below
-# install.packages(c("coda","mvtnorm","devtools","loo"))
-library(devtools)
- devtools::install_github("rmcelreath/rethinking")
+# load the rethinking package which has some useful tools for analyzing stan models
 library(rethinking)
 
-# define a few convenience variables
-N <- 10
+# note that for more than one chain it can be parallelized across chains with:
+# options(mc.cores = parallel::detectCores())
+
+# define a few convenience variables for our fake data
+N <- 50
 S <- 15
 T <- 1
-Nsites <- 2
-sites <- as.numeric(gl(2,5))
+Nsites <- 100
+sites <- as.numeric(gl(Nsites,N))
+
 # fake observed relative abundances
 observed <- matrix(runif(S * Nsites), Nsites, S)
 observed <- sweep(observed, 2, 1:S, '*')
 observed <- sweep(observed, 1, rowSums(observed), '/')
 
 # fake relative abundances from traitspace
-traitspace <- matrix(1.0/S, N, S)
+traitspace <- matrix(1.0/S, Nsites * N, S)
 
 # fake traits
-# note not really used yet
 traits <- matrix(runif(S * T), S, T)
 
 # shove everything important into a named list
 stan.data <- list(
   N = nrow(traitspace),
   S = ncol(traitspace),
-Nsites = nrow(observed),
- sites = sites,
-  # T = ncol(traits),
-  # traits = traits,
+  Nsites = nrow(observed),
+  T = ncol(traits),
+  sites = sites,
+  traits = traits,
   observed = observed,
   traitspace = traitspace
 )
 
-# fit Daniel's stan model
+# fit banquo model in stan
 banquo.fit <- stan(
-  file = "lib/stan/banquo.stan",
+  file = "banquo.stan",
   data = stan.data,
-  iter = 500,
-  chains = 1
+  iter = 1000,
+  chains = 1,
+  # verbose = TRUE,
+  boost_lib = "/usr/include/boost",
+  eigen_lib = "/usr/include/eigen3"
 )
-# fit Daniel's stan model
-banquo_simp.fit <- stan(
-  file = "lib/stan/banquo.stan",
-  data = stan.data,
-  iter = 500,
-  chains = 1
-)
-# take a lot at some of the inferred parameters
-precis(banquo.fit, depth=2, pars=c("logmean_alphaii", "sigma_alphaii", "alpha"))
