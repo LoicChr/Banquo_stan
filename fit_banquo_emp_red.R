b343d8520e39ac87b445461aa9ca1b5c2a520b43
Loïc Chalmandrier <loic_chr@mailoo.org>
2018-08-02 15:55:18 -0600

    Add files via upload

---
+++ fit_banquo_emp_red.R
@@ -0,0 +1,27 @@
+
+rm(list=ls())
+# Librairies
+library(rstan)
+rstan_options(auto_write = TRUE)
+library(rethinking)
+
+
+load("data/stan_data.Rdata")
+
+# fit banquo model in stan
+banquo.fit <- stan(
+  file = "lib/stan3/banquo_tmp.stan",
+  data = stan.data,
+  iter = 20,
+  chains = 1,
+  init = list(list( logmean_alphaii =1,
+                    sigma_alphaii =0.001,
+                    alphaij_intercept =0.01,
+                    alphaij_center =0,
+                    alphaij_width =0.01)) ,
+  verbose = TRUE
+  #   boost_lib = "/usr/include/boost",
+  #  eigen_lib = "/usr/include/eigen3"
+)
+id <- floor(100000*runif(1))
+save(list = ls(), file = paste0("results/results_", id, ".Rdata"))
